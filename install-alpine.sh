5a10e698c16fa41308fce2bb05795b5dc38b3a06
Pedro1234-code <78425126+Pedro1234-code@users.noreply.github.com>
2021-07-23 12:03:17 -0300

    Update install-alpine.sh

--- install-alpine.sh
+++ install-alpine.sh
@@ -10,3 +10,34 @@ sudo mkdir alpine-fs
 
 cd alpine-fs
 
+sudo apt update
+
+sudo apt install wget git -y
+
+sudo dnf install wget git
+
+sudo apk add git wget
+
+arch_url=$uname -m
+
+sudo wget https://github.com/termux/proot-distro/releases/download/v1.2-alpine-rootfs/alpine-minirootfs-3.13.1-$arch_url.tar.gz
+
+sudo tar -xzf alpine-minirootfs-3.13.1-$arch_url.tar.gz
+
+sudo rm -rf alpine-minirootfs-3.13.1-$arch_url.tar.gz
+
+cd /
+
+sudo wget https://raw.githubusercontent.com/Pedro1234-code/alpine-chroot/main/launch.sh
+
+sudo cp /launch.sh /startalpine
+
+sudo cp /startalpine /usr/bin
+
+sudo chmod +x /usr/bin/startalpine
+
+sudo rm -rf /launch.sh
+
+sudo rm -rf /startalpine
+
+echo Finished. Start Alpine with the startalpine command.
